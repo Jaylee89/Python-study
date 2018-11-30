c43227b773faba92d98c6c6b73df8d77110dbfd4
Jaylee89 <121769651@qq.com>
2018-12-01 00:35:59 +0800

    mail add, test script update

--- demo_py3/spider/test.sh
+++ demo_py3/spider/test.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+#linux
 python3 -m venv venv
-# pip3 install --upgrade pip3
+# pip3 install --upgrade pip
 # source venv/bin/activate
+
+#win
+python3 -m venv venv
+cd venv\Scripts
+python3 -m pip install -r requirements.txt
+activate.bat
+cd ../..
